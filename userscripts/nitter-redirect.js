8a5cfbef2df024a0ac5a67c5839e929f3c0b0a10
jocoro19 <132920956+jocoro19@users.noreply.github.com>
2023-06-09 08:56:18 -0600

    Create nitter-redirect.js

---
+++ userscripts/nitter-redirect.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Nitter Redirect
+// @namespace   https://github.com/jocoro19
+// @author		  JoCoRo19
+// @version     1.0
+// @description Redirect Twitter links to a Nitter instance
+// @license		  CCO
+// @icon		    https://nitter.net/apple-touch-icon.png
+// @grant       none
+// @match       *://twitter.com/*
+// @match		    *://www.twitter.com/*
+// @match		    *://mobile.twitter.com/*
+// @homepageURL	https://github.com/jocoro19/filter-lists
+// ==/UserScript==
+
+let nitterInstance = "nitter.net";
+location.replace('https://' + nitterInstance + location.pathname + location.search)
