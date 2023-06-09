7b8d318390f71c199300d8660ece2f85f112e903
jocoro19 <132920956+jocoro19@users.noreply.github.com>
2023-06-09 08:59:47 -0600

    Create old-reddit-redirect.js

---
+++ userscripts/old-reddit-redirect.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Old Reddit Redirect
+// @namespace   https://github.com/jocoro19
+// @author      JoCoRo19
+// @version     1.0
+// @description Redirect all reddit links to Old Reddit
+// @license     CCO
+// @icon        https://www.redditstatic.com/desktop2x/img/favicon/apple-icon-180x180.png
+// @grant       none
+// @match       *://reddit.com/*
+// @match       *://www.reddit.com/*
+// ==/UserScript==
+
+if(location.href.indexOf('www.reddit.com/poll/') != -1 || location.href.indexOf('www.reddit.com/gallery/') != -1) {
+	void 0
+} else {
+	location.replace('https://old.reddit.com' + location.pathname + location.search)
+}
