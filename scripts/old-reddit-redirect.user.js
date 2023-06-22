002b9e0d7bce79b217ceccc3ba027cb526aed96a
jocoro19 <132920956+jocoro19@users.noreply.github.com>
2023-06-21 22:48:18 -0600

    Update old-reddit-redirect.user.js

--- scripts/old-reddit-redirect.user.js
+++ scripts/old-reddit-redirect.user.js
@@ -4,7 +4,7 @@
 // @author      JoCoRo19
 // @version     1.0
 // @description Redirect all reddit links to Old Reddit
-// @license     CC0
+// @license     CC0-1.0
 // @icon        https://www.redditstatic.com/desktop2x/img/favicon/apple-icon-180x180.png
 // @grant       none
 // @match       *://reddit.com/*
