6e7f8c289051112cefd46e7d05889c7ba5082765
jocoro19 <132920956+jocoro19@users.noreply.github.com>
2023-09-07 00:54:05 -0600

    Update twitter.user.js

--- scripts/twitter.user.js
+++ scripts/twitter.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name        JHS X-iter
-// @namespace   https://github.com/jocoro19
-// @author      JoCoRo19
-// @version     1.0
-// @run-at      document-start
-// @description Redirects all Twitter/"X" links to nitter.net and adds a wey to switch to nitter.privacydev.net
-// @grant       none
-// @match       https://twitter.com/*
-// @match       https://x.com/*
-// @match       https://nitter.net/*
-// @match       https://nitter.privacydev.net/*
+// @name          JHS X-iter
+// @namespace     https://github.com/jocoro19
+// @author        JoCoRo19
+// @version       1.0
+// @run-at        document-start
+// @description   Redirects all Twitter/"X" links to nitter.net and adds a wey to switch to nitter.privacydev.net
+// @grant         none
+// @match         https://twitter.com/*
+// @match         https://x.com/*
+// @match         https://nitter.net/*
+// @match         https://nitter.privacydev.net/*
 // ==/UserScript==
 
 const nitter = "nitter.net" // Official, primary instance
