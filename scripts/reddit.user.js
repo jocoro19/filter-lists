5f5681ba500e886f9a78fbad4b067f9b2e34d247
jocoro19 <132920956+jocoro19@users.noreply.github.com>
2023-10-24 04:36:34 -0600

    Update reddit.user.js

--- scripts/reddit.user.js
+++ scripts/reddit.user.js
@@ -2,7 +2,7 @@
 // @name          JHS Reddit Fixes
 // @namespace     https://github.com/jocoro19
 // @author        JoCoRo19
-// @version       1.3
+// @version       1.4
 // @run-at        document-start
 // @description   Redirects all Reddit links to Old Reddit and fixes image links by using a custom image viewer
 // @grant         none
@@ -15,28 +15,33 @@
 // ==/UserScript==
 
 // Config
+const redirect = true // Redirect Reddit links to old.reddit.com
 const autoNsfw = false // Automatically show NSFW posts (Works best for browsing in private tabs)
-const autoOpenImg = true // Automatically expand images and videos in posts
+const autoExpandImg = false // Automatically expand images and videos in posts
 
+// Redirect Reddit links to old.reddit.com
+if (redirect && location.hostname !== "old.reddit.com" && location.pathname !== "/media") {
+	document.title = "reddit: the front page of the internet" // Set title to old default title before redirect is finished
+	location.hostname = "old.reddit.com" // Redirect to Old Reddit if URL is not for a media page
+}
+
+// Automatically show NSFW posts (Works best for browsing in private tabs)
 if (autoNsfw && !(document.cookie.includes("over18"))) {
 	document.cookie = "over18=1;domain=.reddit.com"
 	location.reload()
 }
 
-if (autoOpenImg && location.hostname === "old.reddit.com") {
+// Automatically expand images and videos in posts
+if (autoExpandImg && location.hostname === "old.reddit.com") {
 	window.onload = () => {
 		document.querySelectorAll(".expando-button.video.collapsed").forEach(element => element.click())
 	}
 }
 
-if (location.hostname !== "old.reddit.com" && location.pathname !== "/media") {
-	document.title = "reddit: the front page of the internet" // Set title to old default title before redirect is finished
-	location.hostname = "old.reddit.com" // Redirect to Old Reddit if URL is not for a media page
-}
-
+// Replace Reddit image viewer with something more akin to the browser image viewer
 if (location.hostname !== "old.reddit.com" && location.pathname === "/media") {
-	window.stop() // Stop Reddit page from loading so that a custom one can be created
 	document.title = ""
+	window.stop() // Stop Reddit page from loading any further
 	const rootElement = document.documentElement
 	if (document.head === null) {
 		rootElement.appendChild(document.createElement("head")) // Add head if there isn't a head element
@@ -44,9 +49,20 @@ if (location.hostname !== "old.reddit.com" && location.pathname === "/media") {
 	if (document.body === null) {
 		rootElement.appendChild(document.createElement("body")) // Add body if there isn't a body element
 	}
-	rootElement.removeAttribute("class") // Remove classes from HTML element to prevent breakage
+	rootElement.removeAttribute("class") // Remove classes from <HTML element to prevent breakage
+	rootElement.querySelectorAll('head > :not(title)').forEach(element => element.remove())
 	document.body.innerHTML = "" // Delete everything in the body for now
-	document.head.innerHTML = `<style>
+	const urlParams = new URLSearchParams(window.location.search)
+	const url = urlParams.get("url")
+	const filename = url.match(/[^/]+$/)[0]
+	if (/^https:?\/\/preview\.redd\.it/.test(url)) {
+		// Check if image URL is preview.redd.it and if so, replace with i.redd.it
+		const realFilename = /^[^\?]+/.exec(filename)[0]
+		const realUrl = encodeURIComponent(`https://i.redd.it/${realFilename}`)
+		location.replace(`https://www.reddit.com/media?url=${realUrl}`)
+		return
+	}
+	document.head.insertAdjacentHTML("beforeend", `<style>
 		body { margin: 0 }
 		.overlay { position: fixed; inset: 0; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; margin: 0; overflow: auto; }
 		img.no-zoom { max-height: 100vh; max-width: 100vw; object-fit: contain; margin: 0 auto; }
@@ -54,10 +70,7 @@ if (location.hostname !== "old.reddit.com" && location.pathname === "/media") {
 		img.zoomed-in { margin: auto !important; position: absolute; cursor: zoom-out; }
 		img.zoomed-in.overflow-x { left: 0; }
 		img.zoomed-in.overflow-y { top: 0; }
-	</style><meta name="viewport" content="width=device-width, initial-scale=1.0">`
-	const urlParams = new URLSearchParams(window.location.search)
-	const url = urlParams.get("url")
-	const filename = url.match(/[^/]+$/)[0]
+	</style><meta name="viewport" content="width=device-width, initial-scale=1.0">`)
 	if (url !== null) {
 		const loadImage = new Image()
 		loadImage.src = url
